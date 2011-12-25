25e6f53acee7dc232b6423d63f04cb53d30e17b7
stoke <stoke95@yahoo.it>
2011-12-25 23:54:22 +0100

    Update install.php

--- install.php
+++ install.php
@@ -14,7 +14,7 @@ if (!isset($_POST['user']) || !isset($_POST['pass']) || !isset($_POST['host_user
 } else {
 	mysql_connect($_POST['host'], $_POST['host_user'], $_POST['host_pass']) or die(mysql_error());
 	mysql_select_db($_POST['db']);
-	mysql_query("CREATE TABLE kcms (id INT NOT NULL AUTO_INCREMENT KEY, title VARCHAR(500) NOT NULL, content VARCHAR(1000) NOT NULL )") or die(mysql_error());
+	mysql_query("CREATE TABLE kcms (id INT NOT NULL AUTO_INCREMENT KEY, title VARCHAR(500) NOT NULL, content LONGTEXT NOT NULL )") or die(mysql_error());
 	$fd = fopen("settings.php", "w");
 	fwrite($fd, "<?php\n\$index_id = 1;\n\$my_user = '{$_POST['host_user']}';\n\$my_pass = '{$_POST['host_pass']}';\n\$host = '{$_POST['host']}';\n\$database = '{$_POST['db']}'; \n\$pass = '".md5($_POST['pass'])."';\n\$user = '{$_POST['user']}';\n\$theme = 'default';\n\$opt = array();\n?>");
 	fclose($fd);
